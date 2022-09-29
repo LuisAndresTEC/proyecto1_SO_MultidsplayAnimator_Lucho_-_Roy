c4716d38b9f44ca8a519d0705a825df282711b71
LuisAndresTEC <lrojasmurillo7@estudiantec.cr>
2022-09-28 21:03:57 -0600

    avances en implementaciones

--- proyecto1_SO_MultidsplayAnimator/target/debug/deps/proyecto1_SO_MultidsplayAnimator-b3ef0099982a73d3.d
+++
@@ -1,6 +0,0 @@
-/home/luis/Documentos/Sistemas_operativos/proyecto1/proyecto1_SO_MultidsplayAnimator_Lucho_-_Roy/proyecto1_SO_MultidsplayAnimator/target/debug/deps/proyecto1_SO_MultidsplayAnimator-b3ef0099982a73d3: src/main.rs src/my_pthread.rs
-
-/home/luis/Documentos/Sistemas_operativos/proyecto1/proyecto1_SO_MultidsplayAnimator_Lucho_-_Roy/proyecto1_SO_MultidsplayAnimator/target/debug/deps/proyecto1_SO_MultidsplayAnimator-b3ef0099982a73d3.d: src/main.rs src/my_pthread.rs
-
-src/main.rs:
-src/my_pthread.rs:
