195797badf70a5fc154797fa72e70bb8973fefdc
LuisAndresTEC <lrojasmurillo7@estudiantec.cr>
2022-10-18 21:57:31 -0600

    inicios de parce

--- proyecto1_SO_MultidsplayAnimator/target/debug/deps/cfg_if-f8ad2a765ea7f234.d
+++
@@ -1,7 +0,0 @@
-/home/luis/Documentos/Sistemas_operativos/proyecto1/proyecto1_SO_MultidsplayAnimator_Lucho_-_Roy/proyecto1_SO_MultidsplayAnimator/target/debug/deps/cfg_if-f8ad2a765ea7f234.rmeta: /home/luis/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/luis/Documentos/Sistemas_operativos/proyecto1/proyecto1_SO_MultidsplayAnimator_Lucho_-_Roy/proyecto1_SO_MultidsplayAnimator/target/debug/deps/libcfg_if-f8ad2a765ea7f234.rlib: /home/luis/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/luis/Documentos/Sistemas_operativos/proyecto1/proyecto1_SO_MultidsplayAnimator_Lucho_-_Roy/proyecto1_SO_MultidsplayAnimator/target/debug/deps/cfg_if-f8ad2a765ea7f234.d: /home/luis/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/luis/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
