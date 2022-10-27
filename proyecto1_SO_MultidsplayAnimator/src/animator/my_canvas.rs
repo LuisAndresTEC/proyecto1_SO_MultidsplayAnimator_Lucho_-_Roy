79dbb813ff2a2bfbeb612e61be93e66fd16364e4
Royner39 <roynerarturo39@gmail.com>
2022-10-27 02:04:08 -0600

    Rotación y Modificación Parser

--- proyecto1_SO_MultidsplayAnimator/src/animator/my_canvas.rs
+++ proyecto1_SO_MultidsplayAnimator/src/animator/my_canvas.rs
@@ -3,7 +3,7 @@ use ncurses::{addstr, box_, cbreak, chtype, curs_set, echo, endwin, getch, inits
 use ncurses::CURSOR_VISIBILITY::CURSOR_INVISIBLE;
 use ncurses::ll::werase;
 
-use crate::parser::{languaje, load_file, set_values};
+use crate::parser::{Language, load_file, set_values};
 
 pub(crate) unsafe fn animation(){
     let x = 0;
@@ -45,21 +45,30 @@ pub(crate) unsafe fn animation(){
 
     let mut x_start_index = animation_config.start_position[0];
     let mut y_start_index = animation_config.start_position[1];
-    let mut movements = 1;
+    let movements = 1;
+    let mut time = 0;
+    let mut succes = true;
 
     // Hasta que llegue a la posicion final
-    while x_start_index != animation_config.end_position[0] || y_start_index != animation_config.end_position[1]{
+    while x_start_index != animation_config.end_position[0] || y_start_index != animation_config.end_position[1] {
 
         // Verifica si la figura toca algun borde
-        if (x_start_index == 0 || x_start_index+animation_config.ascii[0].len() as i32 == animation_config.width)  ||
-            (y_start_index == 0 || y_start_index+ascii_size as i32 == animation_config.height) {
+        if (x_start_index == 0 || x_start_index + animation_config.ascii[0].len() as i32 == animation_config.width) ||
+            (y_start_index == 0 || y_start_index + ascii_size as i32 == animation_config.height) {
             werase(monitor);
             wborder(monitor, parse_vertical, parse_vertical, parse_horizontal, parse_horizontal, parse_corner, parse_corner, parse_corner, parse_corner);
 
             mvwprintw(monitor, 1, 1, "Boom! La figura chocó con el borde");
+            succes = false;
             break;
+        } else if time == animation_config.time_execution{
+            werase(monitor);
+            wborder(monitor, parse_vertical, parse_vertical, parse_horizontal, parse_horizontal, parse_corner, parse_corner, parse_corner, parse_corner);
 
-        } else {
+            mvwprintw(monitor, 1, 1, "Boom! Se acabó el tiempo");
+            succes = false;
+            break;
+        }else {
 
             // mover a la derecha
             if x_start_index < animation_config.end_position[0] {
@@ -67,7 +76,7 @@ pub(crate) unsafe fn animation(){
                 wborder(monitor, parse_vertical, parse_vertical, parse_horizontal, parse_horizontal, parse_corner, parse_corner, parse_corner, parse_corner);
 
                 for i in 0..ascii_size {
-                    mvwprintw(monitor, y_start_index + i as i32, x_start_index + movements, &animation_config.ascii[i]);
+                    mvwprintw(monitor, y_start_index + i as i32, x_start_index + movements, &animation_config.ascii_90[i]);
                 }
                 x_start_index = x_start_index + movements;
                 sleep(1);
@@ -79,7 +88,7 @@ pub(crate) unsafe fn animation(){
                 wborder(monitor, parse_vertical, parse_vertical, parse_horizontal, parse_horizontal, parse_corner, parse_corner, parse_corner, parse_corner);
 
                 for i in 0..ascii_size {
-                    mvwprintw(monitor, y_start_index + i as i32, x_start_index - movements, &animation_config.ascii[i]);
+                    mvwprintw(monitor, y_start_index + i as i32, x_start_index - movements, &animation_config.ascii_270[i]);
                 }
                 x_start_index = x_start_index - movements;
                 sleep(1);
@@ -93,7 +102,7 @@ pub(crate) unsafe fn animation(){
                 wborder(monitor, parse_vertical, parse_vertical, parse_horizontal, parse_horizontal, parse_corner, parse_corner, parse_corner, parse_corner);
 
                 for i in 0..ascii_size {
-                    mvwprintw(monitor, y_start_index + movements + i as i32, x_start_index, &animation_config.ascii[i]);
+                    mvwprintw(monitor, y_start_index + movements + i as i32, x_start_index, &animation_config.ascii_180[i]);
                 }
                 y_start_index = y_start_index + movements;
 
@@ -115,32 +124,40 @@ pub(crate) unsafe fn animation(){
 
             }
         }
-
-        mvwprintw(monitor, animation_config.end_position[1]-1, animation_config.end_position[0], "Posicion final! ");
-
-        }
-
-    // rotar ascii
-    let col_len = animation_config.ascii[0].len();
-
-
-    let mut new_ascii:Vec<String> = Vec::new();
-    let mut ascii_aux:String = "".to_string();
-    let mut ascii_index= animation_config.ascii[0].len()-1;
-
-
-    if animation_config.rotation_angle == 90 {
-        
-        for i in 0..ascii_size {
-            let fila = animation_config.ascii[i].clone();
-            let c = fila.chars().nth(ascii_index).unwrap();
-
-            ascii_aux = c.to_string() + &*ascii_aux;
-        }
-        new_ascii.push(ascii_aux);
-
+        time+=1;
     }
 
+    if  succes{
+        werase(monitor);
+        wborder(monitor, parse_vertical, parse_vertical, parse_horizontal, parse_horizontal, parse_corner, parse_corner, parse_corner, parse_corner);
+        wrefresh(monitor);
+        // rotar ascii
+        if animation_config.rotation_angle == 90 {
+
+            for i in 0..ascii_size {
+                mvwprintw(monitor, y_start_index + i as i32, x_start_index, &animation_config.ascii_90[i]);
+            }
+
+        } else if animation_config.rotation_angle == 180 {
+
+            for i in 0..ascii_size {
+                mvwprintw(monitor, y_start_index + i as i32, x_start_index, &animation_config.ascii_180[i]);
+            }
+
+        } else if animation_config.rotation_angle == 270{
+            for i in 0..ascii_size {
+                mvwprintw(monitor, y_start_index + i as i32, x_start_index, &animation_config.ascii_270[i]);
+            }
+        } else {
+            for i in 0..ascii_size {
+                mvwprintw(monitor, y_start_index + i as i32, x_start_index, &animation_config.ascii[i]);
+            }
+
+        }
+        mvwprintw(monitor, animation_config.end_position[1]-1, animation_config.end_position[0], "Posicion final! ");
+    } else{
+
+    }
 
     wrefresh(monitor);
     sleep(5);
--- proyecto1_SO_MultidsplayAnimator/src/parse/parser.rs
+++ proyecto1_SO_MultidsplayAnimator/src/parse/parser.rs
@@ -18,7 +18,7 @@ pub(crate) fn load_file() -> Vec<String> {
 }
 
 
-pub(crate) struct languaje {
+pub(crate) struct Language {
     pub(crate) width: i32,
     pub(crate) height:  i32,
     pub(crate) scheduler: SchedulerEnum,
@@ -29,35 +29,38 @@ pub(crate) struct languaje {
     pub(crate) rotation_angle: i32,
     pub(crate) number_of_monitors: i32,
     pub(crate) monitors:  Vec<String>,
+    pub(crate) ascii_90:  Vec<String>,
+    pub(crate) ascii_180:  Vec<String>,
+    pub(crate) ascii_270:  Vec<String>,
 }
-pub(crate) fn set_values(file: Vec<String>) -> languaje {
+pub(crate) fn set_values(file: Vec<String>) -> Language {
     let mut start_position = Vec::new();
     let mut end_position = Vec::new();
 
     //se intoduce el indice 5 del file a start position como si fueran coordenadas
     let mut start = file[5].split(" ");
-    let mut start_x = start.next().unwrap().parse::<i32>().unwrap();
-    let mut start_y = start.next().unwrap().parse::<i32>().unwrap();
+    let start_x = start.next().unwrap().parse::<i32>().unwrap();
+    let start_y = start.next().unwrap().parse::<i32>().unwrap();
     start_position.push(start_x);
     start_position.push(start_y);
 
     //se intoduce el indice 6 del file a end position como si fueran coordenadas
     let mut end = file[6].split(" ");
-    let mut end_x = end.next().unwrap().parse::<i32>().unwrap();
-    let mut end_y = end.next().unwrap().parse::<i32>().unwrap();
+    let end_x = end.next().unwrap().parse::<i32>().unwrap();
+    let end_y = end.next().unwrap().parse::<i32>().unwrap();
     end_position.push(end_x);
     end_position.push(end_y);
 
     //se crea un vector con los monitores
     let mut monitors = Vec::new();
     monitors.push(file[9].to_string());
-    //se crea un vertor con todos los elementos del ascii
+    //se crea un vector con todos los elementos del ascii
     let mut ascii = Vec::new();
-    for i in 10..17{
+    for i in 10..15{
         ascii.push(file[i].to_string());
     }
     //se parcea de forma manual el nombre del scheduler
-    let mut scheduler;
+    let scheduler;
     if file[3].contains("RoundRobin") {
         scheduler = SchedulerEnum::RoundRobin;
     }else if file[3].contains("Lottery") {
@@ -65,10 +68,22 @@ pub(crate) fn set_values(file: Vec<String>) -> languaje {
     }else {
         scheduler = SchedulerEnum::RealTime;
     }
-
+    // Se definen las rotaciones
+    let mut ascii_90 = Vec::new();
+    for i in 16..21{
+        ascii_90.push(file[i].to_string());
+    }
+    let mut ascii_180 = Vec::new();
+    for i in 22..27{
+        ascii_180.push(file[i].to_string());
+    }
+    let mut ascii_270 = Vec::new();
+    for i in 28..33{
+        ascii_270.push(file[i].to_string());
+    }
 
     //se asignan los valores al struct
-    let mut languaje = languaje {
+    let language = Language {
         width: file[1].parse::<i32>().unwrap(),
         height: file[2].parse::<i32>().unwrap(),
         scheduler: scheduler,
@@ -79,9 +94,12 @@ pub(crate) fn set_values(file: Vec<String>) -> languaje {
         rotation_angle: file[7].parse::<i32>().unwrap(),
         number_of_monitors: file[8].parse::<i32>().unwrap(),
         monitors: monitors,
+        ascii_90: ascii_90,
+        ascii_180: ascii_180,
+        ascii_270: ascii_270,
     };
 
-    return languaje;
+    return language;
 }
 
 
